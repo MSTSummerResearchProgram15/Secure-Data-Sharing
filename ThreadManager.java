d0652902b6ce4a98e47e8bb9bba81c0a18cd0c99
Matt Hartenbower <matt.hartenbower@gmail.com>
2015-07-02 16:52:47 -0500

    Uploaded wrong ThreadManager file

--- ThreadManager.java
+++ ThreadManager.java
@@ -16,14 +16,14 @@ import java.util.concurrent.TimeUnit;
 
 public class ThreadManager {
 	public static char[] array;
-	public static File fin, fout;
+	public static File fin, fin2, fout;
 	
 	public static void main(String[] args) throws IOException{
 		//Generate parameters
 		Params params;
 		ParamsGen gen = new ParamsGen();
 		params = gen.generate();
-		FileSplitMerge sm = new FileSplitMerge();
+		
 		//Preprocessing - split file into chunks
 		fin = new File("test.txt");
 		long fileLength = fin.length();
@@ -35,6 +35,7 @@ public class ThreadManager {
 		}
 		sm.splitFile(fin, splitFileNames, fileSize);
 		
+		
 		//Encrypt the file chunks
         ExecutorService executor = Executors.newFixedThreadPool(5);
         for(int i = 0; i < numFiles; i++){
@@ -45,31 +46,50 @@ public class ThreadManager {
         	Runnable worker = new Encryption(fin, fout, params);
         	executor.execute(worker);
         } 
-        
-        //Decrypt the file chunks
-        for(int j = 0; j < numFiles; j++){
-        	String fileIn = "Encrypted" + j + ".txt";
+
+		// sign file chunks
+		for(int i = 0; i < numFiles; i++){
+			String fileIn = "Encrypted" + i + ".txt";
+			fin = new File(fileIn);
+			String fileOut = "Encrypted" + i + ".txt" + ".signature";
+			fout = new File(fileOut);
+			Runnable worker = new Signer(fin, fout, params);
+			executor.execute(worker);
+		}
+
+		// verify file chunks
+		for(int i = 0; i < numFiles; i++){
+			String fileIn = "Encrypted" + i + ".txt";
+			fin = new File(fileIn);
+			String sigIn = "Encrypted" + i + ".txt" + ".signature";
+			fin2 = new File(sigIn);
+			Runnable worker = new Signer(fin, fin2, params);
+			executor.execute(worker);
+		}
+
+		//Decrypt the file chunks
+        for(int i = 0; i < numFiles; i++){
+        	String fileIn = "Encrypted" + i + ".txt";
         	fin = new File(fileIn);
-      		String fileOut = "Decrypted" + j + ".txt";
+      		String fileOut = "Decrypted" + i + ".txt";
       		fout = new File(fileOut);
         	Runnable worker = new Decryption(fin, fout, params);
         	executor.execute(worker);
         }
         executor.shutdown();
         
-        //wait for threads to finish before continuing
+      //wait for threads to finish before continuing
         try {
 			executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
 		} catch (InterruptedException e) {e.printStackTrace();}
         
-        
-        //Create an array of all files to merge
+        //Post processing - merge the file chunks back into one file
+  		//Create an array of all files to merge
         String[] inputFiles = new String[(int) numFiles];
         for(int i = 0; i < numFiles; i++){
         	inputFiles[i] = "Decrypted" + i + ".txt";
         }
         String outputFile = "Decrypted_Final.txt";
         sm.mergeFiles(inputFiles, outputFile, (int)numFiles);
-        
 	}
 }
