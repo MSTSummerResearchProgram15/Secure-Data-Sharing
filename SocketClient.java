4515cd3cebc58769d80a0640b58e173fbfae5bb2
NeedsMoreGravitas <dyshallow@gmail.com>
2015-07-14 13:20:40 -0500

    fixed try/catch issues

--- SocketClient.java
+++ SocketClient.java
@@ -1,41 +1,53 @@
+
 import java.io.*;
 import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class SocketClient{
+public class SocketClient {
 
-	public void run(){
+    public void run() {
 
-		//Read the port number from a text file
-		BufferedReader br = new BufferedReader(new FileReader("portnumber.txt"));
-		String Port = br.readline();
-		br.close();
+        BufferedReader br;
+        Socket MyClient;
 
-		//Convert string to int 
-		int Portnumber = Integer.parseInt(Port);
+        //Create an input stream
+        DataInputStream input = null;
+        //Create an output stream
+        DataOutputStream output = null;
+        int Portnumber = -1; // initialize to dummy value
 
+        try {
+            //Read the port number from a text file
+            br = new BufferedReader(new FileReader("portnumber.txt"));
+            String Port = br.readLine();
+            br.close();
+
+            //Convert string to int 
+            Portnumber = Integer.parseInt(Port);
+
+        
 		//Open the socket
-		Socket MyClient;
-		MyClient = new Socket("localhost", Portnumber);
+        
+            MyClient = new Socket("localhost", Portnumber);
+            
+            //WRITE WHAT WE WANT TO SEND OR RECEIVE HERE
+            
+            MyClient.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SocketClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+		
+        //Close the input, output stream and thread
+        try {
+            output.close();
+            input.close();
 
-		//Create an input stream
-		DataInputStream input = null;
-		//Create an output stream
-		DataOutputStream output = null;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
-		//WRITE WHAT WE WANT TO SEND OR RECEIVE HERE
-
-
-		//Close the input, output stream and thread
-		try{
-			output.close();
-			input.close();
-			MyClient.close();
-
-		}catch(Exception e)
-		{
-			System.out.println(e);
-		}
-
-	}
+    }
 
 }
