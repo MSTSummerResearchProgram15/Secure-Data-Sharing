c41249e1fe2dd5d74def786a87fdd0e408794540
Matt Hartenbower <matt.hartenbower@gmail.com>
2015-07-24 15:58:18 -0500

    Began FTP integration

--- FTPClient.java
+++ FTPClient.java
@@ -3,27 +3,32 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class FTPClient {
 	public static void main(String[] args) throws Exception {
 
-        long start = System.currentTimeMillis();
-
-        // localhost for testing
-        Socket sock = new Socket("127.0.0.1", 13262);
-        System.out.println("Connecting...");
-        InputStream is = sock.getInputStream();
-        // receive file
-        new FTPClient().receiveFile(is);
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);
-
-        sock.close();
+       
     }
+	
+	public FTPClient() throws UnknownHostException, IOException{
+		 long start = System.currentTimeMillis();
+
+	     // localhost for testing
+	     Socket sock = new Socket("127.0.0.1", 13262);
+	     System.out.println("Connecting...");
+	     InputStream is = sock.getInputStream();
+	     // receive file
+	     long end = System.currentTimeMillis();
+	     System.out.println(end - start);
+
+	     sock.close();
+	}
 
     public void receiveFile(InputStream is) throws Exception {
         int filesize = 6022386;
